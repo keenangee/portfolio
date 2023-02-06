1e8e558379171f245c8fffd3e3b56f6097aa8ebe
Keenan Gee <k33nang33@hotmail.co.uk>
2023-02-06 18:23:11 +0000

    Added css resets and removed unwanted starting code

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-  </div>
 `
 
 setupCounter(document.querySelector('#counter'))
